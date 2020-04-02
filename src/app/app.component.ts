b06f5a4918a2ffda3323db0ed5ace3edecdadd93
Michael Pilote <mpilo0@oceanex.com>
2020-04-02 14:49:54 -0400

    initialCommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { componentTypes } from './componentTypes';
+
+interface IComponent {
+  type: any;
+  quantity: number;
+}
+interface IArtefact {
+  label: string;
+  components: IComponent[];
+  quantity: number;
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public chosenArtefacts: IArtefact[] = [];
+  public neededMaterials: IComponent[] = [];
+  public artefacts: IArtefact[] = [
+    {
+      label: 'Centurion Dress Sword',
+      components: [
+        {
+          type: componentTypes.thirdAgeIron,
+          quantity: 8
+        }
+      ],
+      quantity: 0
+    },
+    {
+      label: 'Venator Dagger',
+      components: [
+        {
+          type: componentTypes.thirdAgeIron,
+          quantity: 12
+        },
+        {
+          type: componentTypes.vellum,
+          quantity: 12
+        }
+      ],
+      quantity: 0
+    }
+  ];
+
+  public addArtefact(p_artefact: IArtefact): void {
+    p_artefact.quantity++;
+    this._calculateTotals();
+  }
+  public removeArtefact(p_artefact: IArtefact): void {
+    p_artefact.quantity--;
+    this._calculateTotals();
+  }
+  public reset(): void {
+    this.artefacts.forEach((p_artefact: IArtefact) => {
+      p_artefact.quantity = 0;
+    });
+    this._calculateTotals();
+  }
+  private _calculateTotals(): void {
+    this.chosenArtefacts = this.artefacts.filter((p_artefact: IArtefact) => {
+      return p_artefact.quantity > 0;
+    });
+    const w_neededMaterials = {};
+    this.chosenArtefacts.forEach((p_artefact: IArtefact) => {
+      p_artefact.components.forEach((p_component) => {
+        if (!w_neededMaterials[p_component.type.id]) {
+          w_neededMaterials[p_component.type.id] = { ...p_component };
+          w_neededMaterials[p_component.type.id].quantity = 0;
+        }
+        w_neededMaterials[p_component.type.id].quantity +=
+          p_component.quantity * p_artefact.quantity;
+      });
+    });
+    console.log(w_neededMaterials);
+    this.neededMaterials = [];
+    for (const w_key in w_neededMaterials) {
+      if (w_neededMaterials.hasOwnProperty(w_key)) {
+        const w_neededMat = w_neededMaterials[w_key];
+        this.neededMaterials.push(w_neededMat);
+      }
+    }
+  }
+}
---
+++ src/app/componentTypes.ts
@@ -0,0 +1,143 @@
+const componentTypes = {
+  thirdAgeIron: {
+    id: 'thirdAgeIron',
+    label: 'Third Age Iron'
+  },
+  zarosianInsigna: {
+    id: 'zarosianInsigna',
+    label: 'Zarosian Insigna'
+  },
+  samiteSilk: {
+    id: 'samiteSilk',
+    label: 'Samite Silk'
+  },
+  imperialSteel: {
+    id: 'imperialSteel',
+    label: 'Imperial Steel'
+  },
+  whiteOak: {
+    id: 'whiteOak',
+    label: 'White Oak'
+  },
+  goldrune: {
+    id: 'goldrune',
+    label: 'Goldrune'
+  },
+  orthenglass: {
+    id: 'orthenglass',
+    label: 'Orthenglass'
+  },
+  vellum: {
+    id: 'vellum',
+    label: 'Vellum'
+  },
+  cadmiumRed: {
+    id: 'cadmiumRed',
+    label: 'Cadmium Red'
+  },
+  ancientVis: {
+    id: 'ancientVis',
+    label: 'Ancient Vis'
+  },
+  tyrianPurple: {
+    id: 'tyrianPurple',
+    label: 'Tyrian Purple'
+  },
+  leatherScraps: {
+    id: 'leatherScraps',
+    label: 'LeatherScraps'
+  },
+  chaoticBrimstone: {
+    id: 'chaoticBrimstone',
+    label: 'ChaoticBrimstone'
+  },
+  demonhide: {
+    id: 'demonhide',
+    label: 'Demonhide'
+  },
+  eyeOfTheDagon: {
+    id: 'eyeOfTheDagon',
+    label: 'Eye Of The Dagon'
+  },
+  hellfireMetal: {
+    id: 'hellfireMetal',
+    label: 'Hellfire Metal'
+  },
+  keramos: {
+    id: 'keramos',
+    label: 'Keramos'
+  },
+  whiteMarble: {
+    id: 'whiteMarble',
+    label: 'White Marble'
+  },
+  cobaltBlue: {
+    id: 'cobaltBlue',
+    label: 'Cobalt Blue'
+  },
+  everlightSilvithril: {
+    id: 'everlightSilvithril',
+    label: 'Everlight Silvithril'
+  },
+  starOfSaradomin: {
+    id: 'starOfSaradomin',
+    label: 'Star Of Saradomin'
+  },
+  bloodOfOrcus: {
+    id: 'bloodOfOrcus',
+    label: 'Blood Of Orcus'
+  },
+  stormguardSteel: {
+    id: 'stormguardSteel',
+    label: 'Stormguard Steel'
+  },
+  wingsOfWar: {
+    id: 'wingsOfWar',
+    label: 'Wings Of War'
+  },
+  animalFurs: {
+    id: 'animalFurs',
+    label: 'Animal Furs'
+  },
+  armadyleanYellow: {
+    id: 'armadyleanYellow',
+    label: 'Armadylean Yellow'
+  },
+  malachiteGreen: {
+    id: 'malachiteGreen',
+    label: 'MalachiteGreen'
+  },
+  markOfTheKyzaj: {
+    id: 'markOfTheKyzaj',
+    label: 'Mark Of The Kyzaj'
+  },
+  vulcanisedRubble: {
+    id: 'vulcanisedRubble',
+    label: 'Vulcanised Rubble'
+  },
+  warforgedBronze: {
+    id: 'warforgedBronze',
+    label: 'WarforgedBronze'
+  },
+  fossilisedBone: {
+    id: 'fossilisedBone',
+    label: 'FossilisedBone'
+  },
+  yubiuskClay: {
+    id: 'yubiuskClay',
+    label: 'Yubiusk Clay'
+  },
+  aetheriumAlloy: {
+    id: 'aetheriumAlloy',
+    label: 'Aetherium Alloy'
+  },
+  quintessence: {
+    id: 'quintessence',
+    label: 'Quintessence'
+  },
+  soapstone: {
+    id: 'soapstone',
+    label: 'Soapstone'
+  }
+};
+export { componentTypes };
