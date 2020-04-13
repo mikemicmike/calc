b66012c7e7ca3429c92fd026b14ff3507032e7e8
Michael Pilote <mpilo0@oceanex.com>
2020-04-13 13:33:59 -0400

    added icons

--- src/app/factions.ts
+++ src/app/factions.ts
@@ -3,31 +3,41 @@ const factions = {
     id: 'zarosian',
     label: 'Zarosian (Kharid-Et)',
     color: '#e375fb',
-    levelReq: 5
+    icon:
+      'https://runescape.wiki/images/thumb/1/11/Zaros_symbol.png/38px-Zaros_symbol.png',
+    levelReq: 5,
   },
   zamorakian: {
     id: 'zamorakian',
     label: 'Zamorakian (Infernal Source)',
     color: '#fd6464',
-    levelReq: 20
+    icon:
+      'https://runescape.wiki/images/thumb/a/af/Zamorak_symbol.png/50px-Zamorak_symbol.png',
+    levelReq: 20,
   },
   saradomist: {
     id: 'saradomist',
     label: 'Saradominist (Everlight)',
     color: '#8fdcf5',
-    levelReq: 42
+    icon:
+      'https://runescape.wiki/images/thumb/9/96/Saradomin_symbol.png/38px-Saradomin_symbol.png',
+    levelReq: 42,
   },
   armadylean: {
     id: 'armadylean',
     label: 'Armadylean (Stormguard Citadel)',
     color: '#f5f96c',
-    levelReq: 70
+    icon:
+      'https://runescape.wiki/images/thumb/5/57/Armadyl_symbol.png/45px-Armadyl_symbol.png',
+    levelReq: 70,
   },
   bandosian: {
     id: 'bandosian',
     label: 'Bandosian (Warforge!)',
     color: '#8DE347',
-    levelReq: 76
-  }
+    icon:
+      'https://runescape.wiki/images/thumb/d/d9/Bandos_symbol.png/25px-Bandos_symbol.png',
+    levelReq: 76,
+  },
 };
 export { factions };
