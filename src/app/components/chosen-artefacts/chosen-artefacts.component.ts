3f42ec6ab8d4320400f4bf0d778e566c3a4af1cd
Matan Kastel <zmtmaster@gmail.com>
2020-04-25 02:31:01 +0300

    Added outfit and relic to exp calulation

--- src/app/components/chosen-artefacts/chosen-artefacts.component.ts
+++ src/app/components/chosen-artefacts/chosen-artefacts.component.ts
@@ -10,6 +10,8 @@ import { IArtefact } from 'src/app/models/IArtefact';
 export class ChosenArtefactsComponent implements OnInit {
   public chosenArtefacts: IArtefact[];
   public totalXp: number = 0;
+  public isOutfit: string = 'none';
+  public isRelic: boolean = false;
   constructor(public store: StoreService) {}
 
   public ngOnInit(): void {
@@ -18,7 +20,30 @@ export class ChosenArtefactsComponent implements OnInit {
       p_artefacts.forEach((p_artefact) => {
         this.totalXp += p_artefact.quantity * p_artefact.xp;
       });
+      this.getTotalXP();
       this.chosenArtefacts = p_artefacts;
     });
+  }
+
+  public toggleisrelic() {
+    this.isRelic = !this.isRelic;
+  }
+
+  public onchangedowndown(value) {
+    this.isOutfit = value;
+  }
+
+  public getTotalXP(): number {
+    if (this.isOutfit === 'outfit' && this.isRelic) {
+      return 1.08 * this.totalXp;
+    }
+    if (this.isOutfit === 'outfit' && !this.isRelic) {
+      return 1.06 * this.totalXp;
+    }
+    if (this.isOutfit === 'none' && this.isRelic) {
+      return 1.02 * this.totalXp;
+    }
+
+    return this.totalXp;
   }
 }
